a98f87d74c567489becdbc1595fbab7706c2efa5
Craig Parker <craig@ab5w.com>
2014-12-30 23:08:44 +0000

    Fixed the youtube_title function and added comments overall.

--- spotify.class.php
+++ spotify.class.php
@@ -4,9 +4,12 @@ class Spotify {
 
     private function check($track) {
 
+        //Input is an array.
+
         //Set the base URL for searching.
         $base = "ws.spotify.com/search/1/track.json";
 
+        //Turn the array into a string.
         $track = implode("+", $track);
 
         //Talk to the spotify API to return some json yumminess.
@@ -87,6 +90,13 @@ class Spotify {
 
     private function youtube_title($url) {
 
+        error_reporting(E_ERROR | E_PARSE);
+
+        //Recontruct the input url to the correct format.
+        $url = parse_url($url);
+        $url = 'https://www.youtube.com/watch?' . $url['query'];
+
+        //Curl the page.
         $ch = curl_init();
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_URL, $url);
@@ -95,12 +105,14 @@ class Spotify {
 
         curl_close($ch);
 
+        //Extract the title from the page contents.
         preg_match("/\<title.*\>(.*)\<\/title\>/", $output, $matches);
 
         $output = $matches[1];
 
         $output = str_replace('- YouTube', '', $output);
         $output = str_replace('&amp;', '', $output);
+        $output = str_replace('&quot;', '', $output);
         $title = preg_replace('/[^A-Za-z0-9\s]/', '', $output);
 
         return $title;
@@ -109,16 +121,20 @@ class Spotify {
 
     private function youtube_to_spotify_title($video_url) {
 
+        //Get the video title from the url.
         $youtube_title = $this->youtube_title($video_url);
 
+        //Create an array from the title.
         $youtube_title_array = array_filter(explode(' ', $youtube_title));
 
         $check = false;
         $count = 0;
         while ($check != true) {
 
+            //If empty, break.
             if (empty($youtube_title_array)) { break; }
 
+            //If no spotify match for the array, remove the last element from the array (and try again).
             if (!$this->check($youtube_title_array)) {
 
                 array_pop($youtube_title_array);
@@ -133,8 +149,10 @@ class Spotify {
 
         }
 
+        //If it took more than four tries to find a match it's most likey a false positive.
         if ($count > 4) { return false; }
 
+        //If the array isn't empty (there was a match), turn the array back into a string (the track name).
         if (!empty($youtube_title_array)) {
 
             $spotify_search = implode(' ', $youtube_title_array);
@@ -151,8 +169,10 @@ class Spotify {
 
     public function youtube($url) {
 
+        //Get the track name from the url.
         $trackname = $this->youtube_to_spotify_title($url);
 
+        //Get the trackid from the track name.
         $trackid = $this->trackid($trackname);
 
         return $trackid;
